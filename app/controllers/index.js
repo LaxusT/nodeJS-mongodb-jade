f4b9cef0dd72a154c5b65d2be2838f19ad7030cf
zhaojinxin1 <zhaojinxin1@wanda.cn>
2017-07-12 15:08:05 +0800

    edit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,3 +15,36 @@ exports.index = function(req, res){
 			});
 		})
 }
+
+// search page
+exports.search = function(req, res){
+	var cateId = req.query.cat;
+	var page = req.query.page;
+	var count = 2;
+	var index = page * count;
+
+	Category
+		.find({_id: cateId})
+		.populate({
+			path: "movies", 
+			select: 'title poster',
+			options: { limit: 5, skip: index }
+		})
+		.exec(function(err, categories){
+			if(err) console.log(err);
+
+			console.log(Math.ceil(categories[0].movies.length / count))
+			var category = categories[0] || {};
+			var movies = category.movies || [];
+			var results = movies.slice(index, index + count);
+
+			res.render("results", { 
+				title: "imooc 结果列表页面",
+				keyword: category.name,
+				currentPage: page + 1,
+				totalPage: Math.ceil(movies.length / count),
+				movies: results,
+				query: 'cat=' + cateId
+			});
+		})
+}
